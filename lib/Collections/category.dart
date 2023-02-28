09e3e0edbcc37c92b1ebd0179e91080ca515adb7
Dharti <dhartic.itpath@gmail.com>
2023-02-28 14:48:40 +0530

    Using Ids & Indexes

--- lib/Collections/category.dart
+++ lib/Collections/category.dart
@@ -2,5 +2,7 @@ import 'package:isar/isar.dart';
 
 @Collection()
 class Category {
+  int id = Isar.autoIncrement;
+  @Index(unique: true)
   late String name;
 }
--- lib/Collections/routine.dart
+++ lib/Collections/routine.dart
@@ -2,8 +2,13 @@ import 'package:isar/isar.dart';
 
 @Collection()
 class Routine {
+  // @Id()
+  int id = Isar.autoIncrement;
   late String title;
+  @Index()
   late DateTime startTime;
+  @Index(caseSensitive: false)
   late String day;
+  @Index(composite: [CompositeIndex('title')])
   late String category;
 }
